3790baec0230ca86d059e4de65c3fbc40ff7090f
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-23 16:12:39 +0000

    items can be marked completed

--- script.js
+++ script.js
@@ -6,6 +6,32 @@ let itemContainer = document.querySelector(".jsItemContainer");
 // When "add" button is clicked -
 add.addEventListener("click", () => {
   addItem();
+
+  // X button removes relative item
+  let deleteBtns = document.querySelectorAll(".jsDeleteItem");
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.parentElement.remove();
+    });
+  });
+  // Check and uncheck an item to mark completion
+  let itemTextContent = document.querySelectorAll(".jsItemText");
+  itemTextContent.forEach((item) => {
+    let isToggled = false;
+    item.addEventListener("click", () => {
+      if (!isToggled) {
+        item.classList.add("strikethrough");
+        item.parentElement.firstChild.firstChild.classList.remove("hidden");
+        item.parentElement.firstChild.classList.add("item-checked");
+        isToggled = true;
+      } else {
+        item.classList.remove("strikethrough");
+        item.parentElement.firstChild.firstChild.classList.add("hidden");
+        item.parentElement.firstChild.classList.remove("item-checked");
+        isToggled = false;
+      }
+    });
+  });
 });
 
 // Adds new item to the page
@@ -23,9 +49,9 @@ function addItem() {
   // Assign classes to elements
   div.classList.add("item");
   checkButton.classList.add("button-check");
-  checkIcon.classList.add("fa-solid", "fa-check");
-  itemText.classList.add("item-text");
-  xButton.classList.add("button-x");
+  checkIcon.classList.add("fa-solid", "fa-check", "hidden");
+  itemText.classList.add("item-text", "jsItemText");
+  xButton.classList.add("button-x", "jsDeleteItem");
   xIcon.classList.add("fa-solid", "fa-x");
   // Item texts text content = input value
   itemText.textContent = inputValue;
